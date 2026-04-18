274e28311eef828e81c177e6fe507722a02e196a
Joaquim <devjoaquim@gmail.com>
2026-04-18 19:16:49 +0200

    Eliminada dependència de delta de _process a nivell.gd

--- scripts/nivell.gd
+++ scripts/nivell.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	# Reacciona als clics de la jugadora
 	if Input.is_action_pressed("peça_frena") and sprite_stamina.pot_frenar():
 		# Frena, augmentant l'interval entre moviments verticals
