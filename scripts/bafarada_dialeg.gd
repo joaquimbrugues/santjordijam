e4776887c84541941e3c22888a0e9812b78e13f3
Joaquim <devjoaquim@gmail.com>
2026-04-23 04:55:16 +0200

    Tots els passos del diàleg

--- scripts/bafarada_dialeg.gd
+++ scripts/bafarada_dialeg.gd
@@ -9,29 +9,27 @@ func _ready() -> void:
 	var tween = get_tree().create_tween()
 	self_modulate = Color.TRANSPARENT
 	tween.tween_property(self, "self_modulate", Color.WHITE, 0.5).set_ease(Tween.EASE_OUT)
-	tween.finished.connect(dispara_primera_linia)
+	tween.finished.connect(dispara_text)
 
 func set_llista_dialeg(llista: Array) -> void:
 	llista_dialeg = llista
 	index = 0
 
-func dispara_primera_linia() -> void:
-	dispara_text(llista_dialeg[0])
-
-func dispara_text(text: String) -> void:
+func dispara_text() -> void:
 	%Boto.set_disabled(true)
-	%TextDialeg.text = text
+	%TextDialeg.text = llista_dialeg[index]
 	%TextDialeg.set_visible_ratio(0.0)
-	index += 1
 	var tween = get_tree().create_tween()
-	tween.tween_property(%TextDialeg, "visible_ratio", 1.0, text.length() / VELOCITAT_ANIMACIO).set_ease(Tween.EASE_OUT)
+	tween.tween_property(%TextDialeg, "visible_ratio", 1.0, llista_dialeg[index].length() / VELOCITAT_ANIMACIO).set_ease(Tween.EASE_OUT)
 	tween.finished.connect(func ():
 		%Boto.set_disabled(false)
 		if index == llista_dialeg.size():
 			# TODO
 			print("Hem de tancar aquesta bafarada!")
 	)
+	index += 1
 
 # Dispara la seguent línia de text
 func _on_boto_pressed() -> void:
-	pass # Replace with function body.
+	if index < llista_dialeg.size():
+		dispara_text()
