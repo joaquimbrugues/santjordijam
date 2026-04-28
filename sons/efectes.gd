2b481cb00a11f267ed01c2974b39e3f44f7d9131
Joaquim <devjoaquim@gmail.com>
2026-04-28 14:26:33 +0200

    Acabat de copiar els fitxers del projecte final

---
+++ sons/efectes.gd
@@ -0,0 +1,13 @@
+extends AudioStreamPlayer
+
+func play_FX(stream: AudioStream, volume = 0.0):
+	var fx_player = AudioStreamPlayer.new()
+	fx_player.stream = stream
+	fx_player.name = "FX_PLAYER"
+	fx_player.volume_db = volume
+	add_child(fx_player)
+	fx_player.play()
+	
+	await fx_player.finished
+	
+	fx_player.queue_free()
